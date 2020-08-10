e7fd1d9c73e81842536c274d54395f46f9704b8c
pat-och <patrickmaige@hotmail.com>
2020-08-10 07:29:50 +0200

    red step CreateFleetCommandHandlerTest

--- tests/acceptance/command/fleet/CreateFleetCommandHandlerTest.php
+++ tests/acceptance/command/fleet/CreateFleetCommandHandlerTest.php
@@ -6,10 +6,34 @@ declare(strict_types=1);
 namespace Tests\acceptance\command\fleet;
 
 use PHPUnit\Framework\TestCase;
+use App\command\shared\app\CommandResponse;
+use App\command\fleet\app\CreateFleetCommandHandler;
+use App\command\fleet\infra\FleetRepositoryInterface;
 
 class CreateFleetCommandHandlerTest extends TestCase
 {
     /**
-     * @todo: ...
+     * @test
      */
+    public function createFleetForFooUserSuccessfully()
+    {
+//        $fooUserId = $this->fooUserExists();
+//
+//        $fleetRepository = new InMemoryFleetRepository();
+//        $this->createFleet($fooUserId, $fleetRepository);
+//
+//        $this->assertNotNull($fleetRepository->getFleet($fooUserId));
+    }
+
+    private function fooUserExists()
+    {
+        return 'foo';
+    }
+
+    private function createFleet(string $fooUserId, FleetRepositoryInterface $fleetRepository)
+    {
+        $commandResponse = new CommandResponse();
+        $createFleetCommandHandler = new CreateFleetCommandHandler($fleetRepository, $commandResponse);
+        $createFleetCommandHandler->handle($fooUserId);
+    }
 }
