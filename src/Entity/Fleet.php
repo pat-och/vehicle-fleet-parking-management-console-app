7d14de25920e1414c958dec13f7b4ea804ec7fa1
pat-och <patrickmaige@hotmail.com>
2020-08-12 18:22:35 +0200

    create Fleet doctrine entity

---
+++ src/Entity/Fleet.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FleetRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=FleetRepository::class)
+ */
+class Fleet
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $uuid;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $user_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUuid(): ?string
+    {
+        return $this->uuid;
+    }
+
+    public function setUuid(string $uuid): self
+    {
+        $this->uuid = $uuid;
+
+        return $this;
+    }
+
+    public function getUserId(): ?string
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(string $user_id): self
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+}
