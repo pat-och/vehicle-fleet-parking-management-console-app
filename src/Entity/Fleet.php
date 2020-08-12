c7717afd113b183a3186bc3dc47ad37d2c5ad135
pat-och <patrickmaige@hotmail.com>
2020-08-12 18:47:06 +0200

    add fleet vehicle ManyToMany relation

--- src/Entity/Fleet.php
+++ src/Entity/Fleet.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\FleetRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -26,6 +28,16 @@ class Fleet
      * @ORM\Column(type="string", length=255)
      */
     private $user_id;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Vehicle::class)
+     */
+    private $Vehicles;
+
+    public function __construct()
+    {
+        $this->Vehicles = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -52,6 +64,32 @@ class Fleet
     public function setUserId(string $user_id): self
     {
         $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Vehicle[]
+     */
+    public function getVehicles(): Collection
+    {
+        return $this->Vehicles;
+    }
+
+    public function addVehicle(Vehicle $vehicle): self
+    {
+        if (!$this->Vehicles->contains($vehicle)) {
+            $this->Vehicles[] = $vehicle;
+        }
+
+        return $this;
+    }
+
+    public function removeVehicle(Vehicle $vehicle): self
+    {
+        if ($this->Vehicles->contains($vehicle)) {
+            $this->Vehicles->removeElement($vehicle);
+        }
 
         return $this;
     }
